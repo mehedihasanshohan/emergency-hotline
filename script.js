d51116c873ffd3363df2c61f53af04ea064b7fce
mehedi hasan <mhshohan04@gmail.com>
2025-08-29 17:18:30 +0600

    dom selections

---
+++ script.js
@@ -0,0 +1,10 @@
+//  dom selection
+const servicesGrid = document.getElementById('services-grid');
+const callHistoryContainer = document.getElementById('call-history-container');
+const clearHistoryBtn = document.getElementById('clear-history-btn');
+const coinsCount = document.getElementById('coins-count');
+const heartsCount = document.getElementById('hearts-count');
+const copyCount = document.getElementById('copy-count');
+const alertModal = document.getElementById('alert-modal');
+const alertMessage = document.getElementById('alert-message');
+const alertOkBtn = document.getElementById('alert-ok-btn');
